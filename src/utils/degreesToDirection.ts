7aff299e372caa82b2e52d2196cf59aca34cd3b7
DenisDov <dovzdenis@gmail.com>
2023-10-20 13:13:41 +0300

    add wind arrows

--- src/utils/degreesToDirection.ts
+++ src/utils/degreesToDirection.ts
@@ -1,13 +1,13 @@
 export function degreesToDirection(degrees: number): string {
   const directions: string[] = [
-    "North",
-    "North East",
-    "East",
-    "South East",
-    "South",
-    "South West",
-    "West",
-    "North West",
+    "North ↓",
+    "North East ↖",
+    "East ←",
+    "South East ↙",
+    "South ↑",
+    "South West ↗",
+    "West →",
+    "North West ↘",
   ];
   const index: number = Math.round(degrees / 45) % 8;
   return directions[index];
